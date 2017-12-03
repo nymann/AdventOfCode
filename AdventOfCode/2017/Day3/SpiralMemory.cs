4e7d76a96a07c1034b2de0693de55dce967306ca
Nymann <s144833@student.dtu.dk>
2017-12-03 08:48:51 +0100

    More clean up.

--- AdventOfCode/2017/Day3/SpiralMemory.cs
+++ AdventOfCode/2017/Day3/SpiralMemory.cs
@@ -5,6 +5,8 @@ namespace AdventOfCode._2017.Day3
 {
     public class SpiralMemory
     {
+        private Point _inputPoint = new Point(0, 0);
+
         public SpiralMemory()
         {
             var input = 368078;
@@ -14,42 +16,6 @@ namespace AdventOfCode._2017.Day3
             Console.WriteLine($"The answer to {GetType().Name} is: {result}.");
             Console.ReadKey();
         }
-
-        /* 5 4 3
-         * 6 1 2 
-         * 7 8 9
-         * 
-         * Length of the array / 2 is the starting point.
-         * 9 / 2 = 4.5 = 4.
-         * arr[4] = 1;
-         * then increment array by 1.
-         */
-
-        /*private int[,] CreateArrayWithBorders(int dimensionSize)
-        {
-            // fill the borders with -1. (a unobtainable value).
-            // dimenson is 3 (eg. 3 by 3 matrix)
-            /*
-             * -1 -1 -1 -1 -1
-             * -1 00 00 00 -1
-             * -1 00 00 00 -1
-             * -1 00 00 00 -1
-             * -1 -1 -1 -1 -1
-             #1#
-
-            // We do this so we don't have to handle edge cases (argumentOutOfRangeExceptions) later on.
-            var arr = new int[dimensionSize + 2, dimensionSize + 2];
-            var length = arr.GetLength(0); // it's a square so getLength(0) and getLength(1) is the same.
-            for (var index = 0; index < length; index++)
-            {
-                arr[index, 0] = -1;
-                arr[0, index] = -1;
-                arr[index, length - 1] = -1;
-                arr[length - 1, index] = -1;
-            }
-
-            return arr;
-        }*/
 
         private int[,] CreateSpiralPatternArray(int input)
         {
@@ -72,10 +38,16 @@ namespace AdventOfCode._2017.Day3
                 {
                     x++;
                     arr[x, y] = number;
+                    if (number == input)
+                    {
+                        _inputPoint.X = x;
+                        _inputPoint.Y = y;
+                    }
                     if (number >= arr.Length)
                     {
                         goto EndThisMisery;
                     }
+
                     number++;
                 }
 
@@ -84,6 +56,12 @@ namespace AdventOfCode._2017.Day3
                 {
                     y--;
                     arr[x, y] = number;
+                    if (number == input)
+                    {
+                        _inputPoint.X = x;
+                        _inputPoint.Y = y;
+                    }
+
                     number++;
                     if (number > arr.Length)
                     {
@@ -99,6 +77,11 @@ namespace AdventOfCode._2017.Day3
                 {
                     x--;
                     arr[x, y] = number;
+                    if (number == input)
+                    {
+                        _inputPoint.X = x;
+                        _inputPoint.Y = y;
+                    }
                     number++;
                     if (number > arr.Length)
                     {
@@ -111,6 +94,12 @@ namespace AdventOfCode._2017.Day3
                 {
                     y++;
                     arr[x, y] = number;
+                    if (number == input)
+                    {
+                        _inputPoint.X = x;
+                        _inputPoint.Y = y;
+                    }
+
                     number++;
                     if (number > arr.Length)
                     {
@@ -128,26 +117,14 @@ namespace AdventOfCode._2017.Day3
             
         private int CalculateManhattanDistance(int[,] spiralArray, int input)
         {
-            var centerSquare = new Point(spiralArray.GetLength(0)/2, spiralArray.GetLength(1) / 2);
-            var inputSquare = new Point(0, 0);
+            var centerPoint = new Point(spiralArray.GetLength(0)/2, spiralArray.GetLength(1) / 2);
 
-            for (var y = 0; y < spiralArray.GetLength(0); y++)
+            if (_inputPoint.X == 0 && _inputPoint.Y == 0)
             {
-                for (var x = 0; x < spiralArray.GetLength(1); x++)
-                {
-                    if (spiralArray[x, y] != input)
-                    {
-                        continue;
-                    }
-
-
-                    inputSquare.X = x;
-                    inputSquare.Y = y;
-                    return Math.Abs(centerSquare.X - inputSquare.X) + Math.Abs(centerSquare.Y - inputSquare.Y);
-                }
+                throw new Exception("Couldn't find an answer to the problem!");
             }
 
-            throw new Exception("Couldn't find an answer to the problem!");
+            return Math.Abs(centerPoint.X - _inputPoint.X) + Math.Abs(centerPoint.Y - _inputPoint.Y); 
         }
     }
 }
