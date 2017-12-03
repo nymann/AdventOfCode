5cadabf0cbfddb70961e3c5a4ab7ca2a154ad201
Nymann <s144833@student.dtu.dk>
2017-12-03 09:53:27 +0100

    Clean up

--- AdventOfCode/2017/Day3/SpiralMemory.cs
+++ AdventOfCode/2017/Day3/SpiralMemory.cs
@@ -5,133 +5,20 @@ namespace AdventOfCode._2017.Day3
 {
     public class SpiralMemory
     {
-        //private Point _inputPoint = new Point(0, 0);
-
         public SpiralMemory()
         {
-            var input = 368078;
-            //var input = 24;
+            const int input = 368078;
             var result = SolutionForPartTwo(input);
-            //var result = CalculateManhattanDistance(arr);
             Console.WriteLine($"The answer to {GetType().Name} is: {result}.");
             Console.ReadKey();
         }
-
-        // Part 1
-        /*private int[,] CreateSpiralPatternArray(int input)
-        {
-            var number = 1;
-            var dimensionSize = (int) Math.Ceiling(Math.Sqrt(input));
-            var arr = new int[dimensionSize, dimensionSize];
-
-            // Start at the center
-            var x = arr.GetLength(0) / 2;
-            var y = arr.GetLength(1) / 2;
-            var travelDistance = 1;
-
-            arr[x, y] = number;
-            number++;
-
-            while (true)
-            {
-                // Go right
-                for (var i = 1; i <= travelDistance; i++)
-                {
-                    x++;
-                    arr[x, y] = number;
-                    if (number == input)
-                    {
-                        _inputPoint.X = x;
-                        _inputPoint.Y = y;
-                    }
-                    if (number >= arr.Length)
-                    {
-                        goto EndThisMisery;
-                    }
-
-                    number++;
-                }
-
-                // Go UP
-                for (var i = 1; i <= travelDistance; i++)
-                {
-                    y--;
-                    arr[x, y] = number;
-                    if (number == input)
-                    {
-                        _inputPoint.X = x;
-                        _inputPoint.Y = y;
-                    }
-
-                    number++;
-                    if (number > arr.Length)
-                    {
-                        goto EndThisMisery;
-                    }
-                }
-
-                // increase travelDistance
-                travelDistance++;
-
-                // Go left
-                for (var i = 1; i <= travelDistance; i++)
-                {
-                    x--;
-                    arr[x, y] = number;
-                    if (number == input)
-                    {
-                        _inputPoint.X = x;
-                        _inputPoint.Y = y;
-                    }
-                    number++;
-                    if (number > arr.Length)
-                    {
-                        goto EndThisMisery;
-                    }
-                }
-
-                // Go down
-                for (var i = 1; i <= travelDistance; i++)
-                {
-                    y++;
-                    arr[x, y] = number;
-                    if (number == input)
-                    {
-                        _inputPoint.X = x;
-                        _inputPoint.Y = y;
-                    }
-
-                    number++;
-                    if (number > arr.Length)
-                    {
-                        goto EndThisMisery;
-                    } 
-                }
-
-                // Increase travel distance
-                travelDistance++;
-            }
-            EndThisMisery:
-
-            return arr;
-        }
-            
-        private int CalculateManhattanDistance(int[,] spiralArray)
-        {
-            var centerPoint = new Point(spiralArray.GetLength(0)/2, spiralArray.GetLength(1) / 2);
-
-            if (_inputPoint.X == 0 && _inputPoint.Y == 0)
-            {
-                throw new Exception("Couldn't find an answer to the problem!");
-            }
-
-            return Math.Abs(centerPoint.X - _inputPoint.X) + Math.Abs(centerPoint.Y - _inputPoint.Y); 
-        }*/
 
         private int SolutionForPartTwo(int input)
         {
             var number = 1;
             var dimensionSize = (int) Math.Ceiling(Math.Sqrt(input));
+            
+            // it's important that we have a center point.
             if (dimensionSize % 2 == 0)
             {
                 dimensionSize++;
@@ -147,6 +34,9 @@ namespace AdventOfCode._2017.Day3
 
             while (true)
             {
+                // The idea is that after each finished combo (right then up), (left then down) the size of the array expands, eg. why we increase the travel distance.
+                // If this doesn't make sense, try to draw a spiral pattern from 1 to 26.
+
                 // Go right
                 for (var i = 1; i <= travelDistance; i++)
                 {
@@ -231,7 +121,7 @@ namespace AdventOfCode._2017.Day3
 
         private int NumberToWrite(int[,] arr, Point point)
         {
-
+            // add the sum of the squares arround the 'point' square.
             var sum = 0;
 
             /*
@@ -240,8 +130,9 @@ namespace AdventOfCode._2017.Day3
              * [0,2] [1,2] [2,2]
              */
 
-            // fx. we are at 1,1
-
+            // fx. we are at [0,0].
+            // then sum = arr[1,0] + arr[1,1] + arr[0,1]
+            
             for (var n = -1; n <= 1; n++)
             {
                 var x = point.X + n;
