294d3e05276ad6c4c827f462bcdcf4bdca65941c
Nymann <s144833@student.dtu.dk>
2017-12-03 04:41:29 +0100

    Added solution for Day1 part 2 and Day2 part 2.

--- AdventOfCode/2017/Day1/InverseCaptcha.cs
+++ AdventOfCode/2017/Day1/InverseCaptcha.cs
@@ -8,12 +8,14 @@ namespace AdventOfCode._2017.Day1
         public InverseCaptcha()
         {
             var input = new Helper.ReadFileLineByLine().FileAsString("C://Users//Nymann//Documents//AdventOfCode//AdventOfCode//2017//Day1//input.txt");
+            //var input = "123425";
             var result = CalculateSumOfMatchingDigits(input);
             Console.WriteLine($"The answer to {GetType().Name} is: {result}.");
             Console.ReadKey();
         }
 
-        private int CalculateSumOfMatchingDigits(string input)
+        // Part 1
+        /*private int CalculateSumOfMatchingDigits(string input)
         {
             var previousDigit = '0';
             var sum = 0;
@@ -31,6 +33,39 @@ namespace AdventOfCode._2017.Day1
             if (input.Last().Equals(input.First()))
             {
                 sum += Convert.ToInt16(input.First().ToString());
+            }
+
+            return sum;
+        }*/
+
+        // Part 2
+        private int CalculateSumOfMatchingDigits(string input)
+        {
+            var sum = 0;
+            var halfwayArround = input.Length / 2;
+
+            for (var index = 0; index < input.Length; index++)
+            {
+                var digit = input[index];
+                var compareIndex = index + halfwayArround;
+
+                // fx {1,2, 1 ,2}, and we are at index 2 (1).
+                // halfwayArround = 4/2 = 2;
+                // compareIndex = 2 + 2 = 4
+                // We need to get to index 0.
+
+                if (compareIndex >= input.Length)
+                {
+                    // How do we 'wrap' arround?
+                    // Modulus!
+                    // compareindex % input.length;
+                    compareIndex %= input.Length;
+                }
+
+                if (digit.Equals(input[compareIndex]))
+                {
+                    sum += Convert.ToInt16(digit.ToString());
+                }
             }
 
             return sum;
