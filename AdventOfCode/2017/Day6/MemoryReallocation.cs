013a4cd8a8c41d305a0001bca922dbad69e13993
Nymann <s144833@student.dtu.dk>
2017-12-06 07:12:52 +0100

    Solution to day 6 added. Part 1: 5042, in (149896 ticks, 46 ms.) Part 2: 1086, in (67260 ticks, 20 ms.)
    Ranks:
    silver: 1667
    Gold: 1606
    
    :sun_with_face: :gun:

---
+++ AdventOfCode/2017/Day6/MemoryReallocation.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2017.Day6
+{
+    public class MemoryReallocation
+    {
+        public object Part1(string input)
+        {
+            var matches = Regex.Matches(input, @"([0-9])+");
+            var blocks = (from Match match in matches
+                select Convert.ToInt32(match.Value)).ToList();
+
+            var histogram =
+                new HashSet<string>
+                {
+                    ListToString(blocks)
+                }; // if we try to add a duplicate, then the histogram.Count won't change since HashSet doesn't allow duplicates.
+
+            while (true)
+            {
+                var maxValue = blocks.Max();
+                var maxIndex = blocks.IndexOf(maxValue);
+                var toDistribute = blocks[maxIndex];
+                var counter = 1;
+                blocks[maxIndex] = 0;
+
+                while (toDistribute > 0)
+                {
+                    blocks[(maxIndex + counter) % blocks.Count]++;
+                    toDistribute--;
+                    counter++;
+                }
+
+                var x = histogram.Count;
+                histogram.Add(ListToString(blocks));
+                if (x == histogram.Count)
+                {
+                    return histogram.Count;
+                }
+            }
+
+            throw new Exception("Couldn't find a solution");
+        }
+
+        public object Part2(string input)
+        {
+            var matches = Regex.Matches(input, @"([0-9])+");
+            var blocks = (from Match match in matches
+                select Convert.ToInt32(match.Value)).ToList();
+
+            var histogram =
+                new HashSet<string>
+                {
+                    ListToString(blocks)
+                }; // if we try to add a duplicate, then the histogram.Count won't change since HashSet doesn't allow duplicates.
+
+            while (true)
+            {
+                var maxValue = blocks.Max();
+                var maxIndex = blocks.IndexOf(maxValue);
+                var toDistribute = blocks[maxIndex];
+                var counter = 1;
+                blocks[maxIndex] = 0;
+
+                while (toDistribute > 0)
+                {
+                    blocks[(maxIndex + counter) % blocks.Count]++;
+                    toDistribute--;
+                    counter++;
+                }
+
+                var x = histogram.Count;
+                var blocksAsString = ListToString(blocks);
+                histogram.Add(blocksAsString);
+                if (x != histogram.Count)
+                {
+                    continue;
+                }
+
+                var index = 0;
+                foreach (var b in histogram)
+                {
+                    if (b.Equals(blocksAsString))
+                    {
+                        return x - index;
+                    }
+                    index++;
+                }
+            }
+
+            throw new Exception("Couldn't find a solution");
+        }
+
+        private string ListToString(List<int> blocks)
+        {
+            return blocks.Aggregate("", (current, block) => current + $" {block}");
+        }
+    }
+}
--- UnitTests/2017/Day6/MemoryReallocationTest.cs
+++ UnitTests/2017/Day6/MemoryReallocationTest.cs
@@ -5,33 +5,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTests._2017.Day6
 {
     [TestClass]
-    public class CurrentProblemTest
+    public class MemoryReallocationTest
     {
         [TestMethod]
         public void Part1Test()
         {
-            var input = new List<string>
-            {
-                "",
-                "",
-            };
+            var input = "0 2 7 0";
 
-            var problem = new CurrentProblem();
+            var problem = new MemoryReallocation();
 
-            Assert.AreEqual(0, problem.Part1(input));
+            Assert.AreEqual(5, problem.Part1(input));
         }
 
         [TestMethod]
         public void Part2Test()
         {
-            var input = new List<string>
+            /*var input = new List<string>
             {
                 "",
                 "",
             };
 
-            var problem = new CurrentProblem();
-            Assert.AreEqual(0, problem.Part2(input));
+            var problem = new MemoryReallocation();
+            Assert.AreEqual(0, problem.Part2(input));*/
         }
     }
 }
